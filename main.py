676f6a4ea5d3a576e0697551e85e2841ba69936d
hugob1234 <159055356+hugob1234@users.noreply.github.com>
2024-02-25 17:06:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-game = 0
-puts "Welcome to the game of Rock, Paper, Scissors!"
-puts "Enter option(play/end):"
-start = gets.chomp()
-guess = false
-while start != "end" and !guess
-    if game != 3
-        array = ["rock","paper","scissors"]
-        computer = array.sample()
-        puts "Enter option(rock/paper/scissors): "
-        player = gets.chomp()
-        game +=1
-    else
-        guess = true
-    end
-    if computer == player
-        puts "Its a tie"
-    elsif computer == "rock" and player == "scissors"
-        puts "Computer won"
-    elsif computer == "paper" and player == "rock"
-        puts "Computer won"
-    elsif computer == "scissors" and player == "paper"
-        puts "Computer won"
+def play_game
+  puts "Welcome to the game of Rock, Paper, Scissors!"
+  puts "Enter option(play/end):"
+  start = gets.chomp()
 
-    else
-        puts "You won!!"
-  break
-    end
-end
-if guess == true or start == "end"
+  if start == "end"
     puts "Adios"
-else
+    return
+  end
+
+  guess = false
+  game = 0
+
+  while game != 3
+    array = ["rock","paper","scissors"]
+    computer = array.sample()
+    puts "Enter option(rock/paper/scissors): "
+    player = gets.chomp()
+    game +=1
+
+    if computer == player
+      puts "It's a tie"
+    elsif (computer == "rock" and player == "scissors") 
+         (computer == "paper" and player == "rock") 
+         (computer == "scissors" && player == "paper")
+      puts "Computer won"
+    else
+      puts "You won!!"
+    end
+
     puts "Good game"
+    guess = true
+  end
 end
+
+play_game
